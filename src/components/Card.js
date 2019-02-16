94716d7caa87f2fd441a38e3b7e72675f398e4cc
Sohel Shaikh <rebht78@gmail.com>
2019-02-16 20:53:22 +0530

    Added Card Component

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Card extends React.Component {
+  render() {
+    return (
+      <section className="card">
+        <h1>Card</h1>
+      </section>
+    );
+  }
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ActionButton from "./components/ActionButton";
+import Card from "./components/Card";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Welcome to THE GAME</h1>
       <ActionButton actiontext="Start Normal Game!" />
-      <ActionButton actiontext="Start Free Game!" />
+      <h2>Coin Available: 10,000</h2>
+      <Card frontImage="" backImage="" />
     </div>
   );
 }
