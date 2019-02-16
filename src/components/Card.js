1229bf09627946d7e35e808b6786e43e18f3e29e
Sohel Shaikh <rebht78@gmail.com>
2019-02-16 21:34:10 +0530

    Added JS files

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 class Card extends React.Component {
-  render() {
+  render(props) {
     return (
-      <section className="card">
-        <h1>Card</h1>
+      <section className="card-section">
+        <div className="card">
+          <img
+            width="200"
+            height="200"
+            src={this.props.frontImage}
+            alt="JS Image"
+          />
+        </div>
       </section>
     );
   }
