516d0dffc648a7957f8e73e66fb7f118dfbc7d50
Sohel Shaikh <rebht78@gmail.com>
2019-02-16 21:45:59 +0530

    added another image for card display

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Card.css";
 
 class Card extends React.Component {
   render(props) {
@@ -6,9 +7,18 @@ class Card extends React.Component {
       <section className="card-section">
         <div className="card">
           <img
+            className="front-face"
             width="200"
             height="200"
+            onClick={this.props.clickhandler}
             src={this.props.frontImage}
+            alt="JS Image"
+          />
+          <img
+            className="back-face"
+            width="200"
+            height="200"
+            src={this.props.backImage}
             alt="JS Image"
           />
         </div>
--- src/index.js
+++ src/index.js
@@ -5,18 +5,24 @@ import Card from "./components/Card";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to THE GAME</h1>
-      <ActionButton actiontext="Start Normal Game!" />
-      <h2>Coin Available: 10,000</h2>
-      <Card
-        frontImage="https://uploads.codesandbox.io/uploads/user/9a910904-cbc5-451e-96a7-077b36805102/mQAw-js.png"
-        backImage=""
-      />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to THE GAME</h1>
+        <ActionButton actiontext="Start Normal Game!" />
+        <h2>Coin Available: 10,000</h2>
+        <Card
+          frontImage="https://uploads.codesandbox.io/uploads/user/9a910904-cbc5-451e-96a7-077b36805102/mQAw-js.png"
+          backImage="https://uploads.codesandbox.io/uploads/user/9a910904-cbc5-451e-96a7-077b36805102/Hu9u-react.png"
+          clickHander={this.onCardClick}
+        />
+      </div>
+    );
+  }
+  onCardClick = event => {
+    alert("Card Clicked!");
+  };
 }
 
 const rootElement = document.getElementById("root");
